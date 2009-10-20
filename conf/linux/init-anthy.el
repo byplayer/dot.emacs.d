61c1875babe8e94715ad84e20a44e189bfebfcd3
yukio <yukio@f9cb7afc-d326-6f48-a1b1-0700e66b032a>
2009-10-20 13:18:04 +0000

    デフォルトフォントがshinonomeに正しくなっていなかったのを変更
    デフォルトフォントがshinonomeに正しくなっていなかったのを変更
    session.*をignore属性へ追加
    anthy-modeの色設定へコメントを追加
    
    
    git-svn-id: http://www.emaki.minidns.net/svn/projects/byplayer/trunk/emacs/dotfiles/.emacs.d@585 f9cb7afc-d326-6f48-a1b1-0700e66b032a

--- conf/linux/init-anthy.el
+++ conf/linux/init-anthy.el
@@ -8,7 +8,7 @@
 
 ;変換時の文字の色
 (set-face-foreground 'anthy-highlight-face "black")
-;アンダーライン消去 
+;アンダーライン消去
 (set-face-underline 'anthy-highlight-face nil)
 (set-face-underline 'anthy-underline-face nil)
 (set-face-background 'anthy-underline-face "dark blue")
--- conf/linux/init-font.el
+++ conf/linux/init-font.el
@@ -1,2 +1,4 @@
 (create-fontset-from-fontset-spec
- "-shinonome-gothic-medium-r-normal--12-*-*-*-*-*-fontset-shinonome12")
+"-shinonome-gothic-medium-r-normal--12-*-*-*-*-*-fontset-shinonome12")
+
+(set-default-font "fontset-shinonome12")

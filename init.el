8f9166f4f2fdfa30e2781e5a7dca993b7ebb8595
byplayer <byplayer100@gmail.com>
2010-11-18 12:11:12 +0900

    stop to show init-loader log after init .

--- init.el
+++ init.el
@@ -6,5 +6,6 @@
 ;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
 ;; デフォルトで"~/.emacs.d/inits"以下のファイルをロードする
 (require 'init-loader)
+(setq init-loader-show-log-after-init nil)
 (init-loader-load)
 

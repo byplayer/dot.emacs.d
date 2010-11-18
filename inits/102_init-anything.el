3390f49011ce26323a9b6c36cf2f72963297a82d
byplayer <byplayer100@gmail.com>
2010-11-18 18:52:07 +0900

    add anything key map and anything sources

--- inits/102_init-anything.el
+++ inits/102_init-anything.el
@@ -35,3 +35,14 @@
 (require 'anything-grep nil t)
 
 (provide 'anything-startup)
+
+(setq anything-sources
+      (list anything-c-source-buffers
+            anything-c-source-bookmarks
+            anything-c-source-file-name-history
+            anything-c-source-man-pages
+            anything-c-source-info-pages
+            anything-c-source-calculation-result
+            anything-c-source-locate))
+
+(define-key global-map (kbd "C-;") 'anything)

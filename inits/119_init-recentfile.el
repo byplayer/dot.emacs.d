c4f9387ab9f9138bfd94de4b9f3e962a761c1ed8
byplayer <byplayer100@gmail.com>
2010-11-18 18:55:31 +0900

    change recent file count and remove recent key map

--- inits/119_init-recentfile.el
+++ inits/119_init-recentfile.el
@@ -3,7 +3,7 @@
 ;====================================
 (require 'recentf)
 (setq recentf-auto-cleanup 'never)
-(setq recentf-max-saved-items 20)
+(setq recentf-max-saved-items 100)
 (setq recentf-exclude '("^/[^/:]+:" "/svn-commit\.tmp$"))
 (recentf-mode 1)
-(global-set-key "\C-xf" 'recentf-open-files)
+; (global-set-key "\C-xf" 'recentf-open-files)

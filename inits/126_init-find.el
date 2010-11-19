12b6f7555487e6ca9828e7a5b0cafad4e1667888
byplayer <byplayer100@gmail.com>
2010-11-19 09:12:57 +0900

    add how to do moccur

--- inits/126_init-find.el
+++ inits/126_init-find.el
@@ -1,3 +1,5 @@
 ;; This file is configuration for grep, find-grep and so on .
 ;; (install-elisp "http://www.emacswiki.org/emacs/download/color-moccur.el")
 ;; (install-elisp "http://www.emacswiki.org/emacs/download/moccur-edit.el")
+;; M-x moccur
+;; type r find window

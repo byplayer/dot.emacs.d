b8580622ceca33b6ebb47b8ba7ae0884c3f65eb7
byplayer <byplayer100@gmail.com>
2010-12-20 11:54:26 +0900

    add auto-complete key

--- inits/127_init-auto-complete.el
+++ inits/127_init-auto-complete.el
@@ -2,3 +2,5 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
 (ac-config-default)
+
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)

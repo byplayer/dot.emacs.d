b2b47471b7f1ea0a45b1efa2b3b76b988b24baea
byplayer <byplayer100@gmail.com>
2010-12-28 16:57:12 +0900

    remove face configuration, because move color-theme

--- inits/123_init-diff.el
+++ inits/123_init-diff.el
@@ -1,22 +1,3 @@
-; set color for diff mode
-(add-hook 'diff-mode-hook
-          (lambda ()
-            (set-face-foreground 'diff-context-face "grey50")
-            (set-face-background 'diff-header-face "black")
-            (set-face-underline-p 'diff-header-face t)
-            (set-face-foreground 'diff-file-header-face "MediumSeaGreen")
-            (set-face-background 'diff-file-header-face "black")
-            (set-face-foreground 'diff-index-face "MediumSeaGreen")
-            (set-face-background 'diff-index-face "black")
-            (set-face-foreground 'diff-hunk-header-face "plum")
-            (set-face-background 'diff-hunk-header-face"black")
-            (set-face-foreground 'diff-removed-face "pink")
-            (set-face-background 'diff-removed-face "gray5")
-            (set-face-foreground 'diff-added-face "light green")
-            (set-face-foreground 'diff-added-face "white")
-            (set-face-background 'diff-added-face "SaddleBrown")
-            (set-face-foreground 'diff-changed-face "DeepSkyBlue1")))
-
 ; add diff file next and diff hunk kill
 (add-hook 'diff-mode-hook
   (lambda()

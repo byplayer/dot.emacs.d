1550ebb873092269c27f238ce09e39269145992a
byplayer <byplayer100@gmail.com>
2011-01-04 12:12:45 +0900

    add ac source and auto start ac mode mode .

--- inits/101_init-auto-complete.el
+++ inits/101_init-auto-complete.el
@@ -2,6 +2,9 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
 (ac-config-default)
+(add-to-list 'ac-sources 'ac-source-yasnippet)
+(add-to-list 'ac-modes 'yaml-mode)
+(add-to-list 'ac-modes 'rhtml-mode)
 
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
 

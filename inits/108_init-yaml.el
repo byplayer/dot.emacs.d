af534174c9ec58c86d88ede8164d54eff681d70d
byplayer <byplayer100@gmail.com>
2011-01-04 11:58:00 +0900

    update yaml-mode

--- inits/108_init-yaml.el
+++ inits/108_init-yaml.el
@@ -1,4 +1,5 @@
 ;; yaml-modeの設定
+(add-to-list 'load-path "~/.emacs.d/elisp/yaml-mode/")
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yml-[a-zA-Z]+$" . yaml-mode))

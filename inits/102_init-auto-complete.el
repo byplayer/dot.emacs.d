0998403c000a116193dd737e96b7fa4e9824981f
byplayer <byplayer100@gmail.com>
2011-01-11 12:16:44 +0900

    remove ac-sources config on init . remove M-TAB binding

--- inits/102_init-auto-complete.el
+++ inits/102_init-auto-complete.el
@@ -2,12 +2,11 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
 (ac-config-default)
-(add-to-list 'ac-sources 'ac-source-yasnippet)
-(add-to-list 'ac-sources 'ac-source-etags)
+
 (add-to-list 'ac-modes 'yaml-mode)
 (add-to-list 'ac-modes 'rhtml-mode)
 
-(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+;; (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
 
 ;; extentions
 (require 'auto-complete-etags)

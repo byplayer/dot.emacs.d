482ffec89a0e3b1653f8676c5bf7fa18ab48ae70
byplayer <byplayer100@gmail.com>
2011-01-13 11:15:19 +0900

    add yasnippet config .

--- 101_init-yasnippet.el
+++ 101_init-yasnippet.el
@@ -1,1 +1,24 @@
-(require 'recentf-ext)
+(add-to-list 'load-path "~/.emacs.d/elisp/yasnippet/")
+(require 'yasnippet)
+(setq yas/trigger-key nil)
+(setq yas/next-field-key nil)
+(setq yas/prev-field-key nil)
+(define-key yas/minor-mode-map (kbd "C-x i i") 'yas/insert-snippet)
+(define-key yas/minor-mode-map (kbd "C-x i f") 'yas/find-snippets)
+(define-key yas/minor-mode-map (kbd "C-x i n") 'yas/new-snippet)
+(define-key yas/minor-mode-map (kbd "C-x i v") 'yas/visit-snippet-file)
+(define-key yas/minor-mode-map (kbd "C-x i e") 'yas/expand)
+;; コメントやリテラルではスニペットを展開しない
+(setq yas/buffer-local-condition
+      '(or (not (or (string= "font-lock-comment-face"
+                             (get-char-property (point) 'face))
+                    (string= "font-lock-string-face"
+                             (get-char-property (point) 'face))))
+           '(require-snippet-condition . force-in-comment)))
+(yas/initialize)
+
+;; anything-c-yasnippet
+(add-to-list 'load-path "~/.emacs.d/elisp/anything-c-yasnippet/")
+(require 'anything-c-yasnippet)
+(setq anything-c-yas-space-match-any-greedy t)
+(global-set-key (kbd "C-c y") 'anything-c-yas-complete)

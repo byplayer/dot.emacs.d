67933977f85ae5d5788ddbde741412470428a7fe
byplayer <byplayer100@gmail.com>
2011-01-14 18:08:10 +0900

    gtags-mode on at php-mode

--- inits/115_init-php.el
+++ inits/115_init-php.el
@@ -15,3 +15,8 @@
     (add-to-list 'auto-mode-alist
                  (cons (car php-file-patterns-temp) 'php-mode))
     (setq php-file-patterns-temp (cdr php-file-patterns-temp))))
+
+(defun my-php-mode-hook()
+  (gtags-mode 1))
+
+(add-hook 'php-mode-hook 'my-php-mode-hook)

19c5d5aac2e35f30e653df294091860dc5cc1731
byplayer <byplayer100@gmail.com>
2011-04-21 11:43:10 +0900

    change gtags symbol regexp for selecting lisp function

--- inits/101_init-gtags.el
+++ inits/101_init-gtags.el
@@ -2,3 +2,7 @@
 (define-key gtags-mode-map "\M-," 'gtags-find-rtag)
 (define-key gtags-mode-map "\M-s" 'gtags-find-symbol)
 (define-key gtags-mode-map "\M-/" 'gtags-find-pattern)
+
+;; change synbol regexp for finding emacs function
+(setq gtags-symbol-regexp "[A-Za-z_][A-Za-z_0-9---]*")
+

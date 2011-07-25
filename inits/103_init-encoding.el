bacfeb3291c360bb45f7555cba0dabae5d1cca61
byplayer <byplayer100@gmail.com>
2011-07-25 17:48:21 +0900

    set auto detect encoding order for rurima

--- inits/103_init-encoding.el
+++ inits/103_init-encoding.el
@@ -2,3 +2,7 @@
 (add-hook 'after-init-hook '(lambda ()
     (setq default-buffer-file-coding-system 'utf-8-unix)
 ))
+
+;; controll order of auto detect encoding
+(prefer-coding-system 'euc-jp-unix)
+(prefer-coding-system 'utf-8-unix)

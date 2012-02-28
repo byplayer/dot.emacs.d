f7c7507125afb4428f59d853728f520d11a1a5ff
byplayer <byplayer100@gmail.com>
2012-02-28 13:32:39 +0900

    fix cedet options

--- inits/003_init-cedet.el
+++ inits/003_init-cedet.el
@@ -1,5 +1,21 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/cedet-1.0.1")
 (load-file "~/.emacs.d/elisp/cedet-1.0.1/common/cedet.el")
-(global-ede-mode 1)
+
+(global-ede-mode t)
+(semantic-load-enable-minimum-features)
 
 (setq semanticdb-default-save-directory "~/.emacs.d/semanticdb/")
+
+;;working with tags
+;; gnu global support
+;; (require 'semanticdb-global)
+;; (semanticdb-enable-gnu-global-databases 'c-mode)
+;; (semanticdb-enable-gnu-global-databases 'c++-mode)
+;; (semanticdb-enable-gnu-global-databases 'ruby-mode)
+
+;; ctags
+(require 'semanticdb-ectag)
+(semantic-load-enable-primary-exuberent-ctags-support)
+
+; (semantic-ectag-add-language-support ruby-mode "ruby" "ruby")
+; (add-hook 'ruby-mode-hook 'semantic-ectag-simple-setup)

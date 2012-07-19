e55e3607c2bcefd440dd4bf09e76c2444521d6dc
yukio.goto <yukio.goto@mail.rakuten.com>
2012-07-19 11:37:03 +0900

    add gtags-mode when js2-mode

--- inits/110_init-html.el
+++ inits/110_init-html.el
@@ -36,3 +36,6 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-hook 'js2-mode-hook
+    (lambda ()
+      (gtags-mode 1)))

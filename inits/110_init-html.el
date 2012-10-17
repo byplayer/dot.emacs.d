0169f29a3e7685b360b8a41560afbcd190c128ba
byplayer <byplayer100@gmail.com>
2012-10-17 16:44:06 +0900

    fix bug of method missing

--- inits/110_init-html.el
+++ inits/110_init-html.el
@@ -41,3 +41,12 @@
              (setq js2-bounce-indent-flag nil)
              (define-key js2-mode-map "\C-m" 'newline-and-indent)
              (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)))
+
+(defun indent-and-back-to-indentation ()
+  (interactive)
+  (indent-for-tab-command)
+  (let ((point-of-indentation
+         (save-excursion
+           (back-to-indentation)
+           (point))))
+    (skip-chars-forward "\s " point-of-indentation)))

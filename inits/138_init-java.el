982f7b449fde9fabf887e8863f35e0856b9d0771
byplayer <byplayer100@gmail.com>
2013-02-27 14:05:02 +0900

    change java-mode style

--- inits/138_init-java.el
+++ inits/138_init-java.el
@@ -1,5 +1,6 @@
 (add-hook 'java-mode-hook
           (lambda ()
+            (c-set-style "java")
             (setq tab-width 4)
             (setq indent-tabs-mode t)
             (setq c-basic-offset 4)))

81816cf8626385ae9a9d7279bf28dd2fa5a32235
byplayer <byplayer100@gmail.com>
2014-03-13 14:01:39 +0800

    set gtags-mode for xml
    for java

--- inits/10-gtags.el
+++ inits/10-gtags.el
@@ -25,6 +25,7 @@
 (add-hook 'ruby-mode-hook 'my-tag-mode-insert-hook)
 (add-hook 'emacs-lisp-mode-hook 'my-tag-mode-insert-hook)
 (add-hook 'lisp-mode-hook 'my-tag-mode-insert-hook)
+(add-hook 'nxml-mode-hook 'my-tag-mode-insert-hook)
 
 (add-hook 'gtags-mode-hook
       '(lambda()

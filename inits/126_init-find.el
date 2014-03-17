9e7043726f805e9346ec97902a9c4f67330e7ff8
byplayer <byplayer100@gmail.com>
2014-03-17 11:32:58 +0800

    remove unused files

--- inits/126_init-find.el
+++
@@ -1,7 +0,0 @@
-;; This file is configuration for grep, find-grep and so on .
-;; (install-elisp "http://www.emacswiki.org/emacs/download/color-moccur.el")
-;; (install-elisp "http://www.emacswiki.org/emacs/download/moccur-edit.el")
-;; M-x moccur
-;; type r find window
-
-;; (install-elisp "http://www.emacswiki.org/emacs/download/grep-edit.el")

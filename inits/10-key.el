bbc177123d187c1d18d7370e63255fb19370b594
byplayer <byplayer100@gmail.com>
2014-03-19 09:41:41 +0800

    rename init file

--- inits/10-key.el
+++ inits/10-key.el
@@ -1,25 +1,24 @@
-;; 改行でnewline
+;;; Code:
+;; Enter as newline
 (global-set-key "\C-m" 'newline-and-indent)
 
-;; homeをVCライクにするための関数
+;; home as Visual C like
 (defun vc-like-home()
   (interactive)
+  (let
+      ((old_pos 0)
+       (new_pos 0))
   (setq old_pos (point))
   (move-beginning-of-line 1)
   (back-to-indentation)
   (setq new_pos (point))
   (if (= old_pos new_pos)
-      (move-beginning-of-line 1)))
+      (move-beginning-of-line 1))))
 
 (global-set-key [home] 'vc-like-home)
 (global-set-key "\C-a" 'vc-like-home)
 
-;; Ctrl+Zで最小化しない
+;; don't minimize Ctrl+Z
 (define-key global-map "\C-z" 'scroll-down)
-
-;; \C-i - [TAB]を解除
-;; 以下を指定するとTABも解除されてしまう・・・
-;; 無念
-; (global-unset-key "\C-i")
 
 (global-unset-key "\M-`")

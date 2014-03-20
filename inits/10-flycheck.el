fbaff6fbd7eee0a9c6b699068df835094b49151e
byplayer <byplayer100@gmail.com>
2014-03-20 15:30:27 +0800

    show flycheck popup soon

--- inits/10-flycheck.el
+++ inits/10-flycheck.el
@@ -1,6 +1,14 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
 ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (eval-after-load 'flycheck
-  '(custom-set-variables
-   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+  '(progn
+     (custom-set-variables
+      '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+     (setq flycheck-display-errors-delay 0.3)))
+
+(provide '10-flycheck)
+;;; 10-flycheck.el ends here

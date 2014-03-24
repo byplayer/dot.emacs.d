89a51aac168d8c5720be0c0fc14100bcec625cf0
byplayer <byplayer100@gmail.com>
2014-03-24 12:50:50 +0800

    add recent ignore list

--- inits/10-recentfile.el
+++ inits/10-recentfile.el
@@ -9,7 +9,7 @@
 (setq recentf-max-saved-items 2000)
 (setq recentf-exclude (append recentf-exclude
                               '("^/[^/:]+:" "/svn-commit\.tmp$" "COMMIT_EDITMSG"
-                                "bookmarks")))
+                                "bookmarks", ".recentf")))
 (setq recentf-auto-cleanup 10)
 (setq recentf-auto-save-timer
       (run-with-idle-timer 30 t 'recentf-save-list))

53a1caeff00f302059137bc85e8b78417bf7fa5c
byplayer <byplayer100@gmail.com>
2014-03-26 16:00:04 +0800

    change java return action

--- inits/10-java.el
+++ inits/10-java.el
@@ -1,6 +1,14 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
 (add-hook 'java-mode-hook
           (lambda ()
             (c-set-style "java")
             (setq tab-width 4)
             (setq indent-tabs-mode nil)
+            (define-key java-mode-map "\C-m" 'c-indent-new-comment-line)
             (setq c-basic-offset 4)))
+
+(provide '10-java)
+;;; 10-java.el ends here

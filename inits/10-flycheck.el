fd92011e3c9cc2313344af038266ede39e86a784
byplayer <byplayer100@gmail.com>
2014-04-29 10:30:40 +0800

    add function to print flycheck current checker

--- inits/10-flycheck.el
+++ inits/10-flycheck.el
@@ -10,5 +10,12 @@
       '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
      (setq flycheck-display-errors-delay 0.3)))
 
+(defun flycheck-print-current-checker (args)
+  "Print checker for current buffer.
+ARGS is dummy"
+  (interactive "P")
+  (print (flycheck-get-checker-for-buffer))
+  )
+
 (provide '10-flycheck)
 ;;; 10-flycheck.el ends here

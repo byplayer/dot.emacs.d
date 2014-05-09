79db0849beeb37cc587bc868af7500a6d215dda6
byplayer <byplayer100@gmail.com>
2014-05-09 17:48:11 +0800

    overwride foodcritic checker

--- inits/10-flycheck.el
+++ inits/10-flycheck.el
@@ -2,6 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 ;; flycheck
+(require 'flycheck)
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (eval-after-load 'flycheck
@@ -15,6 +17,27 @@ ARGS is dummy"
        (interactive "P")
        (print (flycheck-get-checker-for-buffer))
        )))
+
+;; overwride chef-foodcritic checker to set next checkers
+(flycheck-define-checker chef-foodcritic
+  "A Chef cookbooks syntax checker using Foodcritic.
+
+See URL `http://acrmp.github.io/foodcritic/'."
+  :command ("foodcritic" source)
+  :error-patterns
+  ((error line-start (message) ": " (file-name) ":" line line-end))
+  :modes (enh-ruby-mode ruby-mode)
+  :predicate
+  (lambda ()
+    (let ((parent-dir (f-parent default-directory)))
+      (or
+       ;; Chef CookBook
+       ;; http://docs.opscode.com/chef/knife.html#id38
+       (locate-dominating-file parent-dir "recipes")
+       ;; Knife Solo
+       ;; http://matschaffer.github.io/knife-solo/#label-Init+command
+       (locate-dominating-file parent-dir "cookbooks"))))
+  :next-checkers ((warnings-only . ruby-rubocop)))
 
 (provide '10-flycheck)
 ;;; 10-flycheck.el ends here

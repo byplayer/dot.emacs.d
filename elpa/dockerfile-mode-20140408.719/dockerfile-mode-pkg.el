64e12404efbf2a76b289d3d03cfad8fcee231865
byplayer <byplayer100@gmail.com>
2014-05-12 12:43:22 +0800

    add docker file mode

---
+++ elpa/dockerfile-mode-20140408.719/dockerfile-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dockerfile-mode" "20140408.719" "Major mode for editing Docker's Dockerfiles" (quote nil))
--- init.el
+++ init.el
@@ -69,6 +69,9 @@
     git-gutter
     egg
 
+    ;; docker
+    dockerfile-mode
+
     ;; other
     undo-tree
     point-undo

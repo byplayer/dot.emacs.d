04f88f776cb2cd68e7160f9eeec64ae9209a7492
byplayer <byplayer100@gmail.com>
2014-07-11 17:26:11 +0800

    change diff face

--- elisp/color-theme-optional/color-theme-byplayer.el
+++ elisp/color-theme-optional/color-theme-byplayer.el
@@ -37,9 +37,9 @@ Created by byplayer <byplayer100@gmail.com> Dec 27 2010"
      (diff-file-header-face ((t (:foreground "MediumSeaGreen" :background "black" ))))
      (diff-index-face ((t (:foreground "MediumSeaGreen" :background "black" ))))
      (diff-hunk-header-face ((t (:foreground "plum" :background "black" ))))
-     (diff-removed-face ((t (:foreground "gray5"))))
      (diff-added-face ((t (:foreground "white" :background "SaddleBrown" ))))
      (diff-changed-face ((t (:foreground "DeepSkyBlue1"))))
+     (diff-removed-face ((t (:foreground "light gray" :background "#553333"))))
 
      ;; anything
      (anything-file-name ((t (:foreground "yellow green"))))

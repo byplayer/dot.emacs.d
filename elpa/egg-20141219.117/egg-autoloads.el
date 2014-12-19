39ca2bb00c48b575dbf31e53773946bd4b06f916
byplayer <byplayer100@gmail.com>
2014-12-19 17:37:38 +0800

    update egg version

--- elpa/egg-20141219.117/egg-autoloads.el
+++ elpa/egg-20141219.117/egg-autoloads.el
@@ -4,7 +4,7 @@
 
 
 ;;;### (autoloads (egg-minor-mode-find-file-hook egg-minor-mode)
-;;;;;;  "egg" "egg.el" (21501 32508 47215 834000))
+;;;;;;  "egg" "egg.el" (21651 61887 484428 840000))
 ;;; Generated autoloads from egg.el
 
 (autoload 'egg-minor-mode "egg" "\
@@ -37,7 +37,7 @@ egg in current buffer.\\<egg-minor-mode-map>
 ;;;***
 
 ;;;### (autoloads (egg-grep egg-grep-mode egg-grep-process-setup)
-;;;;;;  "egg-grep" "egg-grep.el" (21501 32508 75214 517000))
+;;;;;;  "egg-grep" "egg-grep.el" (21651 61887 488428 790000))
 ;;; Generated autoloads from egg-grep.el
 
 (autoload 'egg-grep-process-setup "egg-grep" "\
@@ -60,7 +60,7 @@ Sets `compilation-last-buffer' and `compilation-window-height'.
 
 ;;;### (autoloads nil nil ("egg-base.el" "egg-const.el" "egg-custom.el"
 ;;;;;;  "egg-diff.el" "egg-git.el" "egg-key.el" "egg-pkg.el" "egg-svn.el")
-;;;;;;  (21501 32508 106273 240000))
+;;;;;;  (21651 61887 521167 867000))
 
 ;;;***
 
---
+++ elpa/egg-20141219.117/egg-pkg.el
@@ -0,0 +1,5 @@
+(define-package "egg" "20141219.117" "Emacs Got Git - Emacs interface to Git" 'nil :keywords
+  '("git" "version control" "release management"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

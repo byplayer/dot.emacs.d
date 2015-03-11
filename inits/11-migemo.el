815f302a01e6e9b99988b8e5beedec81dc442e01
byplayer <byplayer100@gmail.com>
2015-03-11 15:39:12 +0800

    add migemo

---
+++ inits/11-migemo.el
@@ -0,0 +1,18 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+;; migemo
+(require 'migemo nil t)
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
+(setq migemo-coding-system 'utf-8-unix)
+(load-library "migemo")
+(migemo-init)
+
+(provide '11-migemo)
+;;; 11-migemo.el ends here

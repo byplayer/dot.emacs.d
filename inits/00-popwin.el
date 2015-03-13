fc679bb91449fa1856c0191b19d53a41467bfc8d
byplayer <byplayer100@gmail.com>
2015-03-13 13:58:30 +0800

    change popwin config

--- inits/00-popwin.el
+++ inits/00-popwin.el
@@ -6,11 +6,10 @@
 (popwin-mode 1)
 (setq display-buffer-function 'popwin:display-buffer)
 (setq popwin:special-display-config '(("*compilation*" :noselect t)
-                                      ("^\*helm.+\*$" :regexp t :height 0.4)
-                                      ("*my helm*" :regexp t :height 0.4)
+                                      ("*rspec-compilation*" :regexp t)
+                                      ; ("^\*helm.+\*$" :regexp t :height 0.4)
+                                      ; ("*my helm*" :regexp t :height 0.4)
                                       ))
-(push '("*rspec-compilation*" :regexp t) popwin:special-display-config)
-
 (define-key global-map (kbd "C-x p") 'popwin:display-last-buffer)
 
 (provide '00-popwin)

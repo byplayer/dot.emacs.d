307089362a94e19bf317e80e039a46440b541679
byplayer <byplayer100@gmail.com>
2015-03-18 16:36:59 +0800

    add ant-compile command

--- inits/10-java.el
+++ inits/10-java.el
@@ -9,5 +9,17 @@
             (setq indent-tabs-mode nil)
             (setq c-basic-offset 4)))
 
+(defun ant-compile ()
+  "Traveling up the path, find build.xml file and run compile."
+  (interactive)
+  (save-buffer)
+  (with-temp-buffer
+    (while (and (not (file-exists-p "build.xml"))
+        (not (equal "/" default-directory)))
+      (cd ".."))
+    (set (make-local-variable 'compile-command)
+     "ant -emacs")
+    (call-interactively 'compile)))
+
 (provide '10-java)
 ;;; 10-java.el ends here

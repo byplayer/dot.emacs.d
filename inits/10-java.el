ca43f1065e6b603cbfc9bb8bc3d65071c5718afb
byplayer <byplayer100@gmail.com>
2015-04-22 17:53:05 +0800

    support maven

--- inits/10-java.el
+++ inits/10-java.el
@@ -2,16 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun ant-compile ()
+(defun java-compile ()
   "Traveling up the path, find build.xml file and run compile."
   (interactive)
   (save-buffer)
   (with-temp-buffer
-    (while (and (not (file-exists-p "build.xml"))
+    (while (and (not (or (file-exists-p "build.xml")
+                         (file-exists-p "pom.xml")))
         (not (equal "/" default-directory)))
       (cd ".."))
     (set (make-local-variable 'compile-command)
-     "ant -emacs")
+     (if (file-exists-p "build.xml") "ant -emacs" "mvn compile"))
     (call-interactively 'compile)))
 
 (add-hook 'java-mode-hook
@@ -20,7 +21,7 @@
             (setq tab-width 4)
             (setq indent-tabs-mode nil)
             (setq c-basic-offset 4)
-            (define-key java-mode-map (kbd "C-c c") 'ant-compile)))
+            (define-key java-mode-map (kbd "C-c c") 'java-compile)))
 
 (provide '10-java)
 ;;; 10-java.el ends here

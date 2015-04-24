f5974bf0b465a1ba91ece3d38e282632d48747ca
byplayer <byplayer100@gmail.com>
2015-04-24 14:01:55 +0800

    add swoop bindings

--- inits/10-helm.el
+++ inits/10-helm.el
@@ -60,6 +60,10 @@
     "_darcs"             ; Darcs VCS root dir
     ))
 
+(global-set-key (kbd "M-i") 'helm-swoop)
+(global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
 
 (helm-autoresize-mode t)
 

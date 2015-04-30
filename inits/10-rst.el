a39c03abc9e888caeaef50dcb790b6c5312baec6
byplayer <byplayer100@gmail.com>
2015-04-30 11:22:36 +0800

    use rst mode for markdown

--- inits/10-rst.el
+++ inits/10-rst.el
@@ -1,8 +1,5 @@
-(require 'markdown-mode)
 (setq auto-mode-alist
       (append '(
-                ("\\.markdown$" . markdown-mode)
-                ("\\.md$" . markdown-mode)
+                ("\\.markdown$" . rst-mode)
+                ("\\.md$" . rst-mode)
                 ) auto-mode-alist))
-
-(setq markdown-command "bluecloth")

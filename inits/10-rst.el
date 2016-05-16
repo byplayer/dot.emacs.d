ac8dc3345722ca2f6cde570be455394bce6e8ab4
byplayer <byplayer100@gmail.com>
2016-05-16 13:57:28 +0800

    add header and footer

--- inits/10-rst.el
+++ inits/10-rst.el
@@ -1,5 +1,11 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
 (setq auto-mode-alist
       (append '(
                 ("\\.markdown$" . rst-mode)
                 ("\\.md$" . rst-mode)
                 ) auto-mode-alist))
+
+(provide '10-rst)
+;;; 10-rst.el ends here

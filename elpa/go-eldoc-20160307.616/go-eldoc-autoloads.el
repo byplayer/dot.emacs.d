5106549be89b86811fde0ce0ec1b9ead12cc7289
Yukio Goto <yukio.goto@rakuten.com>
2017-11-23 09:38:21 +0800

    upgrade packages

--- elpa/go-eldoc-20160307.616/go-eldoc-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; go-eldoc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (go-eldoc-setup) "go-eldoc" "go-eldoc.el" (22296
-;;;;;;  10852 789348 997000))
-;;; Generated autoloads from go-eldoc.el
-
-(autoload 'go-eldoc-setup "go-eldoc" "\
-Set up eldoc function and enable eldoc-mode.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("go-eldoc-pkg.el") (22296 10852 799044
-;;;;;;  162000))
-
-;;;***
-
-(provide 'go-eldoc-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; go-eldoc-autoloads.el ends here
---
+++ elpa/migemo-20160924.741/migemo-pkg.el
@@ -0,0 +1,1 @@
+(define-package "migemo" "20160924.741" "Japanese incremental search through dynamic pattern expansion" '((cl-lib "0.5")) :commit "e4744efae1b2fdea2bbd2ceaff0f6ea0bb739f5a" :url "https://github.com/emacs-jp/migemo")

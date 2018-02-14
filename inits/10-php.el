71febd3a83a19fd7d261b3a89c6476bef2ff7a0b
byplayer <byplayer100@gmail.com>
2018-02-14 10:58:37 +0800

    fix php config

--- inits/10-php.el
+++ inits/10-php.el
@@ -1,6 +1,7 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; configuration for php
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
 (autoload 'php-mode "php-mode" "PHP mode" t)
 
 (defcustom php-file-patterns (list "\\.php[s34]?\\'" "\\.phtml\\'" "\\.inc\\'")
@@ -13,3 +14,18 @@
     (add-to-list 'auto-mode-alist
                  (cons (car php-file-patterns-temp) 'php-mode))
     (setq php-file-patterns-temp (cdr php-file-patterns-temp))))
+
+(add-hook 'php-mode-hook
+          '(lambda()
+             (setq
+              indent-tabs-mode nil
+              c-default-style "linux"
+              tab-width 4
+              c-basic-offset 4)
+             (c-set-offset 'case-label' 4)
+             (c-set-offset 'arglist-intro' 4)
+             (c-set-offset 'arglist-cont-nonempty' 4)
+             (c-set-offset 'arglist-close' 0)
+             ))
+
+;;; 10-php.el ends here

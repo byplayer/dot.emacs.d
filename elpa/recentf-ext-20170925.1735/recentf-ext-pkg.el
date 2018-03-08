1f5ffc9b372968721ec21e41a40877cde59a8481
byplayer <byplayer100@gmail.com>
2018-03-08 16:14:59 +0800

    Revert "update packages"
    This reverts commit 1d2927d4bf8ca6b1e8e90231f159862de9d7121b.

--- elpa/recentf-ext-20170925.1735/recentf-ext-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "recentf-ext" "20170925.1735" "Recentf extensions" 'nil :commit "450de5f8544ed6414e88d4924d7daa5caa55b7fe" :url "http://www.emacswiki.org/cgi-bin/wiki/download/recentf-ext.el" :keywords '("convenience" "files"))

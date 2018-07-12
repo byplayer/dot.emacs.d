e54c6e891f765c74d686c331787c0f196220df70
byplayer <byplayer100@gmail.com>
2018-07-12 15:14:00 +0800

    update golang env

--- inits/10-golang.el
+++ inits/10-golang.el
@@ -9,11 +9,16 @@
 
      ;; eldoc
      (add-hook 'go-mode-hook 'go-eldoc-setup)
+     (add-hook 'go-mode-hook 'company-mode)
+     (add-hook 'go-mode-hook 'flycheck-mode)
 
      ;; key bindings
      (define-key go-mode-map (kbd "M-.") 'godef-jump)))
 
 (defun my/go-mode-hook ()
   (setq indent-level 4)
-  (setq tab-width 4))
+  (setq c-basic-offset 4)
+  (setq tab-width 4)
+  (add-hook 'before-save-hook' 'gofmt-before-save))
+
 (add-hook 'go-mode-hook 'my/go-mode-hook)

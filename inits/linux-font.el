14fc9adb6bdb5e454a286d249453f7ed9dbff111
byplayer <byplayer100@gmail.com>
2018-09-18 11:15:23 +0800

    change jp font

--- inits/linux-font.el
+++ inits/linux-font.el
@@ -1,4 +1,4 @@
+(set-fontset-font t 'japanese-jisx0208 "TakaoPGothic")
 (create-fontset-from-fontset-spec
-"-shinonome-gothic-medium-r-normal--12-*-*-*-*-*-fontset-shinonome12")
-
+ "-shinonome-gothic-medium-r-normal--12-*-*-*-*-*-fontset-shinonome12")
 (set-frame-font "fontset-shinonome12")

c6f08a2c2501e78892546692098eaab324c3ae11
byplayer <byplayer100@gmail.com>
2018-10-18 09:59:39 +0800

    update package version

---
+++ elpa/company-go-20180427.1856/company-go-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-go" "20180427.1856" "company-mode backend for Go (using gocode)" '((company "0.8.0") (go-mode "1.0.0")) :commit "22f3bf7a9256a30885d7cd46da4657cc878f3f4f" :keywords '("languages") :authors '(("nsf" . "no.smile.face@gmail.com")) :maintainer '("nsf" . "no.smile.face@gmail.com"))
---
+++ elpa/kotlin-mode-20181017.1659/kotlin-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kotlin-mode" "20181017.1659" "Major mode for kotlin" '((emacs "24.3")) :commit "1b6d311168464490ee3a767d36cd9244980e29fe" :keywords '("languages") :authors '(("Shodai Yokoyama" . "quantumcars@gmail.com")) :maintainer '("Shodai Yokoyama" . "quantumcars@gmail.com"))

12193715ec4f7a9a3a91c92cee532025c434b155
byplayer <byplayer100@gmail.com>
2018-10-18 12:03:08 +0800

    change org-capture file name to use date
    Remove open-junk. I will use org-capture instead of junk

--- inits/10-org.el
+++ inits/10-org.el
@@ -5,8 +5,14 @@
 (setq my-org-agenda-directory "~/docs/org/agenda/")
 
 (setq org-directory "~/docs/org")
-(setq org-default-notes-file "captured.org")
 (setq org-agenda-files (list my-org-agenda-directory))
+
+(defun my-org-capture-advice (func &rest args)
+   (let ((org-default-notes-file
+         (format-time-string "~/docs/org/captured/%Y%m%d-%H%M%S.org")))
+    (apply func args)))
+
+(advice-add 'org-capture :around #'my-org-capture-advice)
 
 (global-set-key "\C-ca" 'org-agenda)
 

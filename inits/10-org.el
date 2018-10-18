8f5e65b8f8de05d3306ad41e79e5849eb04c0259
byplayer <byplayer100@gmail.com>
2018-10-18 13:39:53 +0800

    add org-capture template

--- inits/10-org.el
+++ inits/10-org.el
@@ -7,10 +7,11 @@
 (setq org-directory "~/docs/org")
 (setq org-agenda-files (list my-org-agenda-directory))
 
-(defun my-org-capture-advice (func &rest args)
-   (let ((org-default-notes-file
-         (format-time-string "~/docs/org/captured/%Y%m%d-%H%M%S.org")))
-    (apply func args)))
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/docs/org/agenda/gtd.org" "Tasks")
+             "* TODO %?\n")
+        ("m" "Memo" entry (file+datetree "~/docs/org/memo.org")
+             "* %?\nEntered on %T\n")))
 
 (advice-add 'org-capture :around #'my-org-capture-advice)
 

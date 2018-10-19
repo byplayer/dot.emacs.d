d9428cebb007ac336c57f2429764d00a9c28de8c
byplayer <byplayer100@gmail.com>
2018-10-19 10:35:24 +0800

    refactor org-capture template

--- inits/10-org.el
+++ inits/10-org.el
@@ -2,16 +2,31 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq my-org-agenda-directory "~/docs/org/agenda/")
 
 (setq org-directory "~/docs/org")
+(setq my-org-agenda-directory (concatenate 'string org-directory "/agenda"))
 (setq org-agenda-files (list my-org-agenda-directory))
 
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/docs/org/agenda/gtd.org" "Tasks")
+      '(("t" "Todo" entry (file+headline (concatenate 'string my-org-agenda-directory "/todo.org") "Tasks")
              "* TODO %?")
-        ("m" "Memo" entry (file+datetree "~/docs/org/memo.org")
-             "* %?\nEntered on %T\n")))
+        ("m" "Memo" entry (file
+                           (format-time-string
+                            (let (target_dir)
+                              (setq target_dir (format-time-string (concatenate 'string org-directory "/memo/%Y/%Y%m")))
+                              (unless (file-directory-p target_dir)
+                                (make-directory target_dir t))
+                              (concatenate 'string target_dir "/%Y%m%d-%H%M%S.org"))))
+         "* %?\nEntered on %T\n")
+        ("l" "Log" entry (file
+                           (format-time-string
+                            (let (target_dir)
+                              (setq target_dir (format-time-string (concatenate 'string org-directory "/log/%Y/%Y%m")))
+                              (unless (file-directory-p target_dir)
+                                (make-directory target_dir t))
+                              (concatenate 'string target_dir "/%Y%m%d.org"))))
+         "* %?\nEntered on %T\n")
+        ))
 
 (global-set-key "\C-ca" 'org-agenda)
 

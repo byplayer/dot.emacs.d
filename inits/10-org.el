e2b180d03408f2d933fcc011eee2f8b3ccbf3b5e
byplayer <byplayer100@gmail.com>
2018-10-19 11:36:46 +0800

    add suffix for org memo

--- inits/10-org.el
+++ inits/10-org.el
@@ -7,16 +7,18 @@
 (setq my-org-agenda-directory (concatenate 'string org-directory "/agenda"))
 (setq org-agenda-files (list my-org-agenda-directory))
 
+(defun my/generate-org-memo-name ()
+  (let* ((name (read-string "Name: "))
+         (target_dir))
+    (setq target_dir (format-time-string (concatenate 'string org-directory "/memo/%Y/%Y%m")))
+    (unless (file-directory-p target_dir)
+      (make-directory target_dir t))
+    (format "%s/%s-%s.org" target_dir (format-time-string "%Y%m%d-%H%M%S") name )))
+
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline (concatenate 'string my-org-agenda-directory "/todo.org") "Tasks")
              "* TODO %?")
-        ("m" "Memo" entry (file
-                           (format-time-string
-                            (let (target_dir)
-                              (setq target_dir (format-time-string (concatenate 'string org-directory "/memo/%Y/%Y%m")))
-                              (unless (file-directory-p target_dir)
-                                (make-directory target_dir t))
-                              (concatenate 'string target_dir "/%Y%m%d-%H%M%S.org"))))
+        ("m" "Memo" entry (file (my/generate-org-memo-name))
          "* %?\nEntered on %T\n")
         ("l" "Log" entry (file
                            (format-time-string

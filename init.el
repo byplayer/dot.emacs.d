d7bfc9557b18c5231dd4708941e1fb97c627e75c
byplayer <byplayer100@gmail.com>
2018-11-28 09:05:28 +0800

    update auto generated part

--- init.el
+++ init.el
@@ -154,3 +154,29 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode web-mode volatile-highlights undo-tree smartrep smartparens savekill rvm ruby-block rspec-mode rinari rhtml-mode revive recentf-ext rainbow-mode popwin point-undo phpunit php-mode neotree multiple-cursors markdown-mode kotlin-mode js2-mode init-loader helm-swoop helm-projectile helm-migemo helm-ls-git helm-gtags helm-go-package helm-flycheck helm-descbinds helm-c-yasnippet helm-ag groovy-mode go-eldoc go-autocomplete gitignore-mode git-gutter git-commit-mode fuzzy flycheck-pos-tip flycheck-kotlin feature-mode expand-region egg dockerfile-mode crontab-mode company-go color-theme avy anzu ac-ispell ac-helm)))
+ '(term-default-bg-color "#000000")
+ '(term-default-fg-color "light gray")
+ '(truncate-lines nil)
+ '(truncate-partial-width-windows nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(web-mode-comment-face ((t (:foreground "#D9333F"))))
+ '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-rule-face ((t (:foreground "#A0D8EF"))))
+ '(web-mode-doctype-face ((t (:foreground "#82AE46"))))
+ '(web-mode-html-attr-name-face ((t (:foreground "#C97586"))))
+ '(web-mode-html-attr-value-face ((t (:foreground "#82AE46"))))
+ '(web-mode-html-tag-face ((t (:foreground "#E6B422" :weight bold))))
+ '(web-mode-server-comment-face ((t (:foreground "#D9333F")))))

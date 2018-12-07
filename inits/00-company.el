a8b8eb1e1183329dcd48f550511dd67354d2f50a
byplayer <byplayer100@gmail.com>
2018-12-07 11:31:57 +0800

    need to fix

--- inits/00-company.el
+++ inits/00-company.el
@@ -27,7 +27,7 @@
 ;; company-mode for all mode C-M-i
 (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
 
-(add-to-list 'company-backends 'company-yasnippet)
+; (add-to-list 'company-backends 'company-yasnippet)
 
 (provide '00-company)
 ;;; 00-company.el ends here

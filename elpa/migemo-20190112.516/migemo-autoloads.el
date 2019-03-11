16880de095fc36416499d48ca1451a1e43c8757d
byplayer <byplayer100@gmail.com>
2019-03-11 09:35:28 +0800

    update lbes

---
+++ elpa/migemo-20190112.516/migemo-autoloads.el
@@ -0,0 +1,15 @@
+;;; migemo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("migemo.el") (23685 47022 742160 98000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; migemo-autoloads.el ends here

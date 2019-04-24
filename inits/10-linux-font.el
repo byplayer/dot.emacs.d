effed911821639b2d3e0fb1bce9474cf673665cd
byplayer <byplayer100@gmail.com>
2019-04-24 12:39:33 +0800

    fix warning

--- inits/10-linux-font.el
+++ inits/10-linux-font.el
@@ -1,4 +1,10 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
 (set-fontset-font t 'japanese-jisx0208 "TakaoPGothic")
 (create-fontset-from-fontset-spec
  "-shinonome-gothic-medium-r-normal--12-*-*-*-*-*-fontset-shinonome12")
 (set-frame-font "fontset-shinonome12")
+
+;;; 10-linux-font.el ends here

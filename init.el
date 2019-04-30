c04182f4d87f173764d423e0fa12979f6ceac497
byplayer <byplayer100@gmail.com>
2019-04-30 16:43:17 +0800

    remove unused path

--- init.el
+++ init.el
@@ -185,7 +185,6 @@
 
 ;; load-path
 (add-to-list 'load-path "~/.emacs.d/elisp")
-(add-to-list 'load-path "~/.emacs.d/auto-install")
 
 ;; init-loader
 ;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el

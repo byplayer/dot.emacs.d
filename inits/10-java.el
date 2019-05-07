bf5b35b98901faab86cab4cfbb48b76be51f2cc6
byplayer <byplayer100@gmail.com>
2019-05-07 11:00:16 +0800

    add google-java-format

--- inits/10-java.el
+++ inits/10-java.el
@@ -28,9 +28,25 @@
             (setq c-basic-offset 4)
             (setq tab-width 4)
             (setq indent-tabs-mode nil)
-            (define-key java-mode-map (kbd "C-c c") 'java-compile)))
+            (define-key java-mode-map (kbd "C-c c") 'java-compile))
+          )
 
 (add-to-list 'auto-mode-alist '("\\.gradle$"     . groovy-mode))
+
+;; google-java-format
+(add-to-list 'load-path "~/.emacs.d/elisp/google-java-format/")
+(require 'google-java-format)
+(setq google-java-format-executable "/opt/google-java-format/bin/google-java-format")
+
+;; google-java-format-buffer
+(defun my-java-before-save ()
+  "Usage: (add-hook 'before-save-hook #'my-java-before-save)"
+
+  (interactive)
+  (if (eq major-mode 'java-mode))
+      (google-java-format-buffer))
+(add-hook 'before-save-hook #'my-java-before-save)
+
 
 (provide '10-java)
 ;;; 10-java.el ends here

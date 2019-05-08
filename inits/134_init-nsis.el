c6f6441333e6461463b356dd8cda6faccd707a1e
byplayer <byplayer100@gmail.com>
2019-05-08 11:02:53 +0800

    remove nsis-mode

--- inits/134_init-nsis.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'load-path "~/.emacs.d/elisp/nsis-mode/")
-(autoload 'nsis-mode "nsis-mode" "NSIS mode" t)
-(setq auto-mode-alist (append '(("\\.\\([Nn][Ss][Ii]\\)$" .
-                                 nsis-mode)) auto-mode-alist))
-(setq auto-mode-alist (append '(("\\.\\([Nn][Ss][Hh]\\)$" .
-                                 nsis-mode)) auto-mode-alist))

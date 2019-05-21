aff9696ee1d1eb63373213c64b2b8b20e9d54b03
byplayer <byplayer100@gmail.com>
2019-05-21 16:20:42 +0800

    remove git-commit-mode
    Because git-commit-mode was conflict C-c C-(c|k) key bindings
    with magit commit action.

--- inits/10-git.el
+++ inits/10-git.el
@@ -1,16 +1,6 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
-(use-package git-commit-mode
-  :ensure t
-  :init
-  (defun my-git-commit-mode-hook()
-    (progn
-      (setq buffer-undo-list nil)
-      (flyspell-mode 1)
-      (turn-on-auto-fill)))
-  (add-hook 'git-commit-mode-hook 'my-git-commit-mode-hook))
-
 (use-package gitignore-mode
   :ensure t
   :defer t)

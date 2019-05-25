10d016e8b3917f7242002d14530dc60cd8f8d995
byplayer <byplayer100@gmail.com>
2019-05-25 09:54:20 +0800

    add direct push/pull upstream key bind for magit-mode
    Reduce type count to push/pull

--- inits/10-git.el
+++ inits/10-git.el
@@ -14,8 +14,9 @@
   :ensure t
   :bind (("C-x g" . magit-status)
          :map magit-mode-map
-         ("M-c" . magit-commit-create))
-  )
+         ("M-c" . magit-commit-create)
+         ("S-M-p" . magit-push-current-to-upstream)
+         ("S-M-f" . magit-pull-from-upstream)))
 
 (provide '10-git)
 ;;; 10-git.el ends here

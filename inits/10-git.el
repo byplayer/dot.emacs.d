05915bb7f2c26fca1d6d06939bcdb404037dc258
byplayer <byplayer100@gmail.com>
2019-05-25 10:05:50 +0800

    fix bind key configuration for magit-mode
    S-M-p -> M-P
    S-M-f -> M-F

--- inits/10-git.el
+++ inits/10-git.el
@@ -15,8 +15,8 @@
   :bind (("C-x g" . magit-status)
          :map magit-mode-map
          ("M-c" . magit-commit-create)
-         ("S-M-p" . magit-push-current-to-upstream)
-         ("S-M-f" . magit-pull-from-upstream)))
+         ("M-P" . magit-push-current-to-upstream)
+         ("M-F" . magit-pull-from-upstream)))
 
 (provide '10-git)
 ;;; 10-git.el ends here

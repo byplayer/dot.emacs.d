84d9b2d26ad727aab74045fc886b3e605ee2cc95
byplayer <byplayer100@gmail.com>
2019-05-30 15:28:19 +0800

    add clone sub tree short cut

--- inits/10-org.el
+++ inits/10-org.el
@@ -115,6 +115,7 @@
 
   (add-to-list 'org-speed-commands-user '("d" org-todo "DONE"))
   (add-to-list 'org-speed-commands-user '("c" org-todo "CANCEL"))
+  (add-to-list 'org-speed-commands-user '("C" org-clone-subtree-with-time-shift))
   (add-to-list 'org-speed-commands-user '("D" org-deadline ""))
   (add-to-list 'org-speed-commands-user '("A" my/org-archive-this-file))
   (add-to-list 'org-speed-commands-user '("s" org-schedule ""))

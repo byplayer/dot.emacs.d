1dd6f2d09ab6312fa79d17f7ef96b1d3e78ae8e9
byplayer <byplayer100@gmail.com>
2019-07-29 15:37:58 +0800

    remove unused font configuration

--- inits/10-linux-font.el
+++ inits/10-linux-font.el
@@ -2,7 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-fontset-font t 'japanese-jisx0208 "TakaoPGothic")
 (create-fontset-from-fontset-spec
  "-shinonome-gothic-medium-r-normal--12-*-*-*-*-*-fontset-shinonome12")
 (set-frame-font "fontset-shinonome12")

d4689eea93cea07b2f41b136342a93436aee1d97
byplayer <byplayer100@gmail.com>
2019-09-03 15:09:57 +0800

    :sparkles: Add python environment

--- inits/10-python.el
+++ inits/10-python.el
@@ -3,9 +3,20 @@
 ;;; Code:
 
 (use-package python
-  :ensure)
+  :ensure t
+  :mode (("\\.py$" . python-mode)))
 
-;; setup elpy someday
+(use-package elpy
+  :ensure t
+  :init
+  (setq elpy-rpc-backend "jedi")
+  (elpy-enable))
+
+(use-package py-autopep8
+  :ensure t
+  :commands py-autopep8-before-save
+  :init
+  (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
 
 (provide '10-python)
 ;;; 10-python.el ends here

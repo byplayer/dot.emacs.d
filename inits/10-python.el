3079f234b39f9af38cd8e650c1bbdef77a94ae84
byplayer <byplayer100@gmail.com>
2019-09-03 16:06:55 +0800

    :recycle: Revert ":sparkles: Add python environment"
    Remove elpy
    
    This reverts commit d4689eea93cea07b2f41b136342a93436aee1d97.

--- inits/10-python.el
+++ inits/10-python.el
@@ -3,20 +3,9 @@
 ;;; Code:
 
 (use-package python
-  :ensure t
-  :mode (("\\.py$" . python-mode)))
+  :ensure)
 
-(use-package elpy
-  :ensure t
-  :init
-  (setq elpy-rpc-backend "jedi")
-  (elpy-enable))
-
-(use-package py-autopep8
-  :ensure t
-  :commands py-autopep8-before-save
-  :init
-  (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
+;; setup elpy someday
 
 (provide '10-python)
 ;;; 10-python.el ends here

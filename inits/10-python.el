86f041d752a5489e95dd4987effb9be755798efb
byplayer <byplayer100@gmail.com>
2019-09-03 16:16:42 +0800

    :+1: Add py-autopep8
    To auto format for python code

--- inits/10-python.el
+++ inits/10-python.el
@@ -3,9 +3,14 @@
 ;;; Code:
 
 (use-package python
-  :ensure)
+  :ensure t
+  :mode (("\\.py$" . python-mode)))
 
-;; setup elpy someday
+(use-package py-autopep8
+  :ensure t
+  :commands py-autopep8-before-save
+  :init
+  (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
 
 (provide '10-python)
 ;;; 10-python.el ends here

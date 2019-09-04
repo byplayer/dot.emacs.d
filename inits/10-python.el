2c54df6cccd898bf86ef1bc4ebc15f940d20f2ae
byplayer <byplayer100@gmail.com>
2019-09-04 15:41:42 +0800

    :+1: Add virtualenvwrapper autoload commands
    Set commands to load virtualenvwrapper

--- inits/10-python.el
+++ inits/10-python.el
@@ -8,12 +8,19 @@
 
 (use-package py-autopep8
   :ensure t
-  :commands py-autopep8-before-save
+  :commands (py-autopep8-before-save py-autopep8-enable-on-save)
   :init
   (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
 
 (use-package virtualenvwrapper
-  :ensure t)
+  :commands (venv-projectile-auto-workon
+             venv-workon
+             venv-mkvirtualenv-using
+             venv-mkvirtualenv
+             venv-rmvirtualenv
+             venv-lsvirtualenv
+             venv-cdvirtualenv
+             venv-cpvirtualenv))
 
 (provide '10-python)
 ;;; 10-python.el ends here

9cae03ec4354afd282c20aa424d4419fc44566bc
byplayer <byplayer100@gmail.com>
2019-09-05 11:58:32 +0800

    :sparkles: Add auto-virtualenvwrapper and jedi completion
    To use auto-virtualenvwrapper and jedi completion in python

---
+++ elpa/auto-virtualenvwrapper-20181006.937/auto-virtualenvwrapper-autoloads.el
@@ -0,0 +1,28 @@
+;;; auto-virtualenvwrapper-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "auto-virtualenvwrapper" "auto-virtualenvwrapper.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from auto-virtualenvwrapper.el
+
+(autoload 'auto-virtualenvwrapper-activate "auto-virtualenvwrapper" "\
+Activate virtualenv for buffer-filename.
+
+\(fn)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "auto-virtualenvwrapper" '("auto-virtualenvwrapper-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-virtualenvwrapper-autoloads.el ends here
--- inits/10-python.el
+++ inits/10-python.el
@@ -12,7 +12,27 @@
   :init
   (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
 
+(use-package jedi-core
+  :ensure t
+  :commands (jedi:start-dedicated-server
+             helm-jedi-related-names
+             jedi:setup
+             jedi:install-server
+             jedi:reinstall-server
+             jedi:install-server-block)
+  :init
+  (setq jedi:environment-root "~/.python_tool")
+  (setq python-environment-directory "python_tool"))
+
+(use-package company-jedi
+  :ensure t
+  :init
+  (defun my/python-mode-hook ()
+    (add-to-list 'company-backends 'company-jedi))
+  (add-hook 'python-mode-hook 'my/python-mode-hook))
+
 (use-package virtualenvwrapper
+  :ensure t
   :commands (venv-projectile-auto-workon
              venv-workon
              venv-mkvirtualenv-using
@@ -21,6 +41,11 @@
              venv-lsvirtualenv
              venv-cdvirtualenv
              venv-cpvirtualenv))
+(use-package auto-virtualenvwrapper
+  :ensure t
+  :commands (auto-virtualenvwrapper-activate)
+  :init
+  (add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate))
 
 (provide '10-python)
 ;;; 10-python.el ends here

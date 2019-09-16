37a60f4111fc560c246d878b12853062106baf64
byplayer <byplayer100@gmail.com>
2019-09-16 17:28:23 +0800

    :+1: Change font normal to medium

--- inits/10-linux-font.el
+++ inits/10-linux-font.el
@@ -2,8 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Source Han Code JP N" :height 80)
-(set-frame-font "Source Han Code JP N")
-
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 80)
+(set-frame-font "Source Han Code JP M")
 
 ;;; 10-linux-font.el ends here
--- inits/hosts/vagrantn.el
+++ inits/hosts/vagrantn.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Source Han Code JP N" :height 90)
-(set-frame-font "Source Han Code JP N")
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 90)
+(set-frame-font "Source Han Code JP M")
 
 ;;; vagrantn.el ends here

85bbec3b0ec6f97d8d6202e551ad44fed7f4f22d
byplayer <byplayer100@gmail.com>
2019-10-08 09:26:52 +0800

    :arrow_up: Update elpa packages

--- elpa/clang-format-20180406.1514/clang-format-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clang-format" "20180406.1514" "Format code using clang-format" '((cl-lib "0.3")) :commit "98ff1c7ae89f524192cdb6870d000c79bcf7596f" :keywords '("tools" "c"))

4d56d72d9ce13b5bd3b84d877a83e7e0bb111ebe
byplayer <byplayer100@gmail.com>
2019-11-13 22:59:52 +0800

    :+1: Change font size to support new Mac Book Air

--- inits/10-linux-font.el
+++ inits/10-linux-font.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Source Han Code JP M" :height 80)
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 140)
 (set-frame-font "Source Han Code JP M")
 
 ;;; 10-linux-font.el ends here

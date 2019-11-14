3d29e3ac6c407cc610219c755c24fc01e4707d43
byplayer <byplayer100@gmail.com>
2019-11-14 11:58:19 +0800

    :+1: increase font size to fit new pc

--- inits/10-linux-font.el
+++ inits/10-linux-font.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Source Han Code JP M" :height 140)
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 160)
 (set-frame-font "Source Han Code JP M")
 
 ;;; 10-linux-font.el ends here

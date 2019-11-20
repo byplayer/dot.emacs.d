2943826c8fdd30c3db8f7abacc9e6db36e7cdbb4
byplayer <byplayer100@gmail.com>
2019-11-20 15:14:19 +0800

    :arrow_up: Update elpa packages

--- elpa/org-sticky-header-20190407.713/org-sticky-header-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-sticky-header" "20190407.713" "Show off-screen Org heading at top of window" '((emacs "24.4") (org "8.3.5")) :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :url "http://github.com/alphapapa/org-sticky-header")

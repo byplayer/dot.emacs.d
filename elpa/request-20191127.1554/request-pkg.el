fc04d9071705af07ad10357a8c8b9b7adbb8a846
byplayer <byplayer100@gmail.com>
2019-12-18 00:21:56 +0800

    :arrow_up: Update packages

--- elpa/request-20191127.1554/request-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "request" "20191127.1554" "Compatible layer for URL request in Emacs" '((emacs "24.4")) :commit "01753650e6532730bd64ad774f811d626d6eb02a" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")

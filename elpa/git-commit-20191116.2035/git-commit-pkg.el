0d4749f306c72f38e1a5d45f9b90c4040f334468
byplayer <byplayer100@gmail.com>
2020-01-21 11:58:47 +0900

    update packages

--- elpa/git-commit-20191116.2035/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20191116.2035" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "af612063a557eaa4920ff9d30636d524c013ed5d" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")

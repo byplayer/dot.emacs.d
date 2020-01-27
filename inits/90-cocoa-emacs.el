08f8c597022bfed1d285b0cee2f94f8d4a92b9bd
byplayer <byplayer100@gmail.com>
2020-01-27 16:13:04 +0900

    :+1: Add font configuration for mac
    Target font size is different from linux environment.
    
    Change font size on mac environment.

---
+++ inits/90-cocoa-emacs.el
@@ -0,0 +1,8 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 110)
+(set-frame-font "Source Han Code JP M")
+
+;;; 90-cocoa-emacs.el ends here

97c86e3941eaf3a1594cc1786ffc3a59eac91959
byplayer <byplayer100@gmail.com>
2020-01-30 15:26:51 +0900

    :+1: Set fonts a little bit bigger
    It is too small for me.

--- inits/90-cocoa-emacs.el
+++ inits/90-cocoa-emacs.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Source Han Code JP M" :height 100)
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 110)
 (set-frame-font "Source Han Code JP M")
 
 ;;; 90-cocoa-emacs.el ends here

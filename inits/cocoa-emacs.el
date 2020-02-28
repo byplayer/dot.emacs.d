7b9840cd71fd179772377999c751b9567caa3b9d
byplayer <byplayer100@gmail.com>
2020-02-29 00:58:07 +0900

    :sparkles: uncomment for cocoa-emacs related code

--- inits/cocoa-emacs.el
+++ inits/cocoa-emacs.el
@@ -2,9 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (set-face-attribute 'default nil :family "Source Han Code JP M" :height 110)
-;; (set-frame-font "Source Han Code JP M")
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 110)
+(set-frame-font "Source Han Code JP M")
 
-;; (setq ndeb-program-name "/opt/eblook/bin/eblook")
+(setq ndeb-program-name "/opt/eblook/bin/eblook")
 
 ;;; 90-cocoa-emacs.el ends here

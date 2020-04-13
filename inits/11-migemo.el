7c0aab2796b2af998da803a40cc4418b8b745cc8
byplayer <byplayer100@gmail.com>
2020-04-13 09:35:52 +0900

    :+1: Fix dictionary path for mac

--- inits/11-migemo.el
+++ inits/11-migemo.el
@@ -9,9 +9,10 @@
 
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
-(setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
+; (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
+;; Mac
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
 (setq migemo-coding-system 'utf-8-unix)
-(load-library "migemo")
 (migemo-init)
 
 (provide '11-migemo)

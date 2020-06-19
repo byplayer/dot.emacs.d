cb4a2eeacda23c3dec6809278e7c549a4b51b850
byplayer <byplayer100@gmail.com>
2020-06-19 10:23:05 +0900

    :+1: remove revive el.
    Integrate revive configuration to 10-misc.el.

--- inits/10-misc.el
+++ inits/10-misc.el
@@ -9,6 +9,10 @@
 (autoload 'save-current-configuration "revive" "Save status" t)
 (autoload 'resume "revive" "Resume Emacs" t)
 (autoload 'wipe "revive" "Wipe emacs" t)
+(define-key ctl-x-map "F" 'resume)                        ; C-x F resume
+(define-key ctl-x-map "K" 'wipe)                          ; C-x K kill
+(add-hook 'kill-emacs-hook 'save-current-configuration)   ; auto save
+(resume) ; auto resume
 
 ;; regin
 (require 'expand-region)

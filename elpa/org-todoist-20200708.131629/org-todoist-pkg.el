e69005caa347e2df4eef11a7755b429e1ae23527
byplayer <byplayer100@gmail.com>
2020-07-08 13:17:57 +0900

    :arrow_up: Update packages

---
+++ elpa/org-todoist-20200708.131629/org-todoist-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-todoist" "20200708.131629" "Org sync with todoist" '((request-deferred "20181129") (emacs "26")) :authors '(("byplayer" . "byplayer100@gmail.com")) :maintainer '("byplayer" . "byplayer100@gmail.com") :url "https://github.com/byplayer/org-todoist")

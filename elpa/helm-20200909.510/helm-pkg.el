7ab38b1b87f21af67776a4006e698fd057cd07c8
byplayer <byplayer100@gmail.com>
2020-09-14 10:41:21 +0900

    :arrow_up: Update packages

--- elpa/helm-20200909.510/helm-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "helm" "20200909.510" "Helm is an Emacs incremental and narrowing framework"
-  '((emacs "25.1")
-    (async "1.9.4")
-    (popup "0.5.3")
-    (helm-core "3.6.5"))
-  :commit "76fb819030694174c19e4ebec0ecf7952f15768a" :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:

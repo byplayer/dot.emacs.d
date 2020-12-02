d77f8b368eaf5b5ab8d2a7843f451171623de802
byplayer <byplayer100@gmail.com>
2020-12-02 09:56:08 +0900

    :fire: rename 10-html.el to 10-web.el
    The filename html expects for only html but it includes js and json.

--- inits/10-web.el
+++ inits/10-web.el
@@ -48,7 +48,7 @@
 
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+
 (add-hook 'js2-mode-hook
           '(lambda ()
              (require 'js)

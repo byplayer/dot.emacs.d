cef1a63f33c8cc1db82da0cd5f918106c8eae986
byplayer <byplayer100@gmail.com>
2021-01-05 01:02:43 +0900

    Revert ":+1: add leaf"
    This reverts commit 965afd4519fc3521d81750ac2d4e07a4096d1123.

--- elpa/leaf-20201211.412/leaf-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/yukio.goto/.emacs.d/elpa/leaf-20201211.412/leaf.el  -*- no-byte-compile: t -*-
-(define-package "leaf" "20201211.412" "Simplify your init.el configuration, extended use-package" '((emacs "24.1")) :commit "e0c4b7484ab6ee3bbf8413f620ccb99af4328d2f" :authors '(("Naoya Yamashita" . "conao3@gmail.com")) :maintainer '("Naoya Yamashita" . "conao3@gmail.com") :keywords '("lisp" "settings") :url "https://github.com/conao3/leaf.el")

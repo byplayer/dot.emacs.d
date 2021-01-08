7a71fb973483f05d19ad90d5e76dbce2726255fe
byplayer <byplayer100@gmail.com>
2021-01-08 14:43:42 +0900

    :+1: remove unused quelpa and quelpa-use-package

--- elpa/quelpa-use-package-20201022.746/quelpa-use-package-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/yukio.goto/.emacs.d/elpa/quelpa-use-package-20201022.746/quelpa-use-package.el  -*- no-byte-compile: t -*-
-(define-package "quelpa-use-package" "20201022.746" "quelpa handler for use-package" '((emacs "25.1") (quelpa "1.0") (use-package "2")) :commit "d985c0326b66aa19581918deccdc5edcacccf953" :keywords '("package" "management" "elpa" "use-package") :authors '(("steckerhalter")) :maintainer '("steckerhalter") :url "https://github.com/quelpa/quelpa-use-package")

60bb4e04c74e61791be90ebc65eada7625315f82
byplayer <byplayer100@gmail.com>
2021-01-18 23:15:18 +0900

    :arrow_up: Update elpa packages

---
+++ elpa/dash-20210118.912/dash-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dash" "20210118.912" "A modern list library for Emacs"
+  '((emacs "24"))
+  :commit "7fc72d91719ae9585cdbd323031d0144596e1248" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("extensions" "lisp")
+  :url "https://github.com/magnars/dash.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

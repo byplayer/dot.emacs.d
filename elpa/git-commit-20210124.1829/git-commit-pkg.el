72b38b3cad9c5a9183d7c32f4d70e885fe653873
byplayer <byplayer100@gmail.com>
2021-02-04 12:13:13 +0900

    :arrow_up: Update elpa package

---
+++ elpa/git-commit-20210124.1829/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/yukio.goto/.emacs.d/elpa/git-commit-20210124.1829/git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20210124.1829" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "e80eff30d24337a75e07f6d2c154f7d82f140d28" :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
---
+++ elpa/lsp-mode-20210202.1453/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20210202.1453" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "01bcb3f29ba50ad9029a42fea1abf82ad71750b7" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

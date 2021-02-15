5d07742eb5b378815785297d5a336c5cf106723d
Yukio <yukio.goto@rakuten.com>
2021-02-15 10:08:34 +0900

    :+1: Increase mac font size

--- inits/cocoa-emacs-conf.el
+++ inits/cocoa-emacs-conf.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Source Han Code JP M" :height 110)
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 120)
 (set-frame-font "Source Han Code JP M")
 
 (setq ndeb-program-name "/opt/eblook/bin/eblook")

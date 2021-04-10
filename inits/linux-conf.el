6cfb069a6be9cb96456b522c285a199c84b45d91
byplayer <byplayer100@gmail.com>
2021-04-11 01:35:20 +0900

    👍 Change font size to use low HDI mode

--- inits/linux-conf.el
+++ inits/linux-conf.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Source Han Code JP M" :height 170)
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 90)
 (set-frame-font "Source Han Code JP M")
 
 (cond (window-system

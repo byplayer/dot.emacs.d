d033596376ec7546b61c48c17376a99e20a3d819
byplayer <byplayer100@gmail.com>
2021-04-12 09:04:30 +0900

    👍 one more smaller font for linux

--- inits/linux-conf.el
+++ inits/linux-conf.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Source Han Code JP M" :height 90)
+(set-face-attribute 'default nil :family "Source Han Code JP M" :height 80)
 (set-frame-font "Source Han Code JP M")
 
 (cond (window-system

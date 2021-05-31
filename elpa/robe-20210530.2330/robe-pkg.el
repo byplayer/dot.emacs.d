439f7534ef7e512ea48006af0eaae7fd0b54d924
byplayer <byplayer100@gmail.com>
2021-05-31 16:45:19 +0900

    ⬆️ Update elpa packages

---
+++ elpa/robe-20210530.2330/robe-pkg.el
@@ -0,0 +1,13 @@
+(define-package "robe" "20210530.2330" "Code navigation, documentation lookup and completion for Ruby"
+  '((inf-ruby "2.5.1")
+    (emacs "24.4"))
+  :commit "62fc724f9c767930133cb602b0a89190272d267a" :authors
+  '(("Dmitry Gutov"))
+  :maintainer
+  '("Dmitry Gutov")
+  :keywords
+  '("ruby" "convenience" "rails")
+  :url "https://github.com/dgutov/robe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

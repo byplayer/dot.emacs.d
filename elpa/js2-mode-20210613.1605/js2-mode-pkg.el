cbdfc8b3d8b708407ab2107042d0c3975340dbbb
byplayer <byplayer100@gmail.com>
2021-06-18 15:14:49 +0900

    ⬆️ update elpa packages

---
+++ elpa/js2-mode-20210613.1605/js2-mode-pkg.el
@@ -0,0 +1,15 @@
+(define-package "js2-mode" "20210613.1605" "Improved JavaScript editing mode"
+  '((emacs "24.1")
+    (cl-lib "0.5"))
+  :commit "5e9515dff26ed2fa382c97b0b67cdd370f7859f4" :authors
+  '(("Steve Yegge" . "steve.yegge@gmail.com")
+    ("mooz" . "stillpedant@gmail.com")
+    ("Dmitry Gutov" . "dgutov@yandex.ru"))
+  :maintainer
+  '("Steve Yegge" . "steve.yegge@gmail.com")
+  :keywords
+  '("languages" "javascript")
+  :url "https://github.com/mooz/js2-mode/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

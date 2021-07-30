455990826ac37a2677dd8954792b58af1ce3eab4
byplayer <byplayer100@gmail.com>
2021-07-30 10:22:17 +0900

    ⬆️ Update elpa packages
    Signed-off-by: byplayer <byplayer100@gmail.com>

---
+++ elpa/helm-core-20210716.1933/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20210716.1933" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "2c6839f32afc5056b1045f21a1b9771452dfa8f9")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-ls-git-20210729.911/helm-ls-git-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from helm-ls-git.el  -*- no-byte-compile: t -*-
+(define-package "helm-ls-git" "20210729.911" "list git files." '((helm "1.7.8")) :commit "d861fb407d72470db41ac458447b92c6d9b00206")

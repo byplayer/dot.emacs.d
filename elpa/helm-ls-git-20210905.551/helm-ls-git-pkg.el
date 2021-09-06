8d79883dc3cac3ee6ca0b6430eaab8c523c8bab9
byplayer <byplayer100@gmail.com>
2021-09-06 09:29:45 +0900

    ⬆️Update elpa packages

---
+++ elpa/helm-ls-git-20210905.551/helm-ls-git-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from helm-ls-git.el  -*- no-byte-compile: t -*-
+(define-package "helm-ls-git" "20210905.551" "list git files." '((helm "1.7.8")) :commit "aaa9b04fa72e468fac68cee7ab5ed7a0e7d3ec64")
---
+++ elpa/js2-mode-20210830.12/js2-mode-pkg.el
@@ -0,0 +1,15 @@
+(define-package "js2-mode" "20210830.12" "Improved JavaScript editing mode"
+  '((emacs "24.1")
+    (cl-lib "0.5"))
+  :commit "b913961e410dbfdc52a80d62eb4cfe7a305b4e3e" :authors
+  '(("Steve Yegge" . "steve.yegge@gmail.com")
+    ("mooz" . "stillpedant@gmail.com")
+    ("Dmitry Gutov" . "dgutov@yandex.ru"))
+  :maintainer
+  '("Steve Yegge" . "steve.yegge@gmail.com")
+  :keywords
+  '("languages" "javascript")
+  :url "https://github.com/mooz/js2-mode/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
